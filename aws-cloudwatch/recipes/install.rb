bf6c6e0f06efb181dd89cc996e3ec2d15819d684
Nayan <36026937+nayanen@users.noreply.github.com>
2019-08-30 16:04:59 +0530

    Update install.rb

--- aws-cloudwatch/recipes/install.rb
+++ aws-cloudwatch/recipes/install.rb
@@ -46,12 +46,12 @@ end
 #execute 'run this' do
  #  command "sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json -s"
 #end
-template "#{node['aws_cloudwatch']['path']}/etc/amazon-cloudwatch-agent.json" do
-   source 'awslogs.conf.erb'
-end
+
 
 execute 'run this' do
-   command "sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json -s"
+  creates "#{node['aws_cloudwatch']['path']}/etc/amazon-cloudwatch-agent.json"
+  source 'awslogs.conf.erb'
+  command "sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -a fetch-config -m ec2 -c file:/opt/aws/amazon-cloudwatch-agent/etc/amazon-cloudwatch-agent.json -s"
 end
 
 # restart the agent service in the end to ensure that
